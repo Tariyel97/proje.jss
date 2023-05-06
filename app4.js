85251a71c308f704aceb8dfeeb4ccddd3c774fdb
Tariyel Sayilov <sayilovtariyel97@gmail.com>
2023-05-06 18:13:06 +0400

    add. proje

---
+++ app4.js
@@ -0,0 +1,61 @@
+// task1
+ // const product ={
+  //   name: 'exsaple',
+  //   price:1200 ,
+                     
+                    
+  // }
+ // product.price -=200
+ // console.log(product.price)
+           
+      // const product = {
+     //   name:'honor',
+    //   price: 2400,
+   //   owner : 'ali',
+  //   ownerCountry : {
+ //     name: "Azerbaijan",
+ //     capital: "Baku"
+ //   }
+   //     key: 'jony'
+  // }
+     // console.log (product.price)
+
+    //  task2
+
+      // let sum = 0
+                      // let salaries = {
+                      //   john: 100 ,
+                      //   ann: 160 ,
+                      //   peter : 130,
+                      // }
+                      // for (let key in salaries ){
+                      //   sum+=salaries[key]
+                      // }
+                      //  console.log(sum)
+
+
+        // task4
+         // let n ='ali'
+         // let n2 =n1
+          //   n1 = 'Ahmad'
+         //   console.log (n1)
+         //   console.log(n2)
+         //   let d1 ={ad: 'Ali'}
+         //   let d2 = d1 
+         //   d1.ad = 'ahmad'
+        //       console.log(d1.ad)
+        //       console.log(d2.ad)
+
+
+        // task5
+         // const sezar = {
+                      //     name: 'sezar',
+                      //     age:2005
+                      // }
+                      //   // spread
+                      //   // const sezar2 = Object.assign({}, sezar)
+
+                      //   const sezar2 = {...sezar}
+                      //   sezar.name = 'yuli'
+                      //   console.log(sezar2)
+
