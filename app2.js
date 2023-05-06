740a495c7bed3df89131bfb8919d8dd965f564e8
Tariyel Sayilov <sayilovtariyel97@gmail.com>
2023-05-06 17:46:45 +0400

    add commit

---
+++ app2.js
@@ -0,0 +1,63 @@
+            // task1
+
+// let ed = 15
+//   if( ed %5 === 0 && ed % 3 ===0){
+//     console.log('Fizzzbuzz')
+//   }else if ( ed % 5=== 0){
+//      console.log('Fizz')
+//   }else if (ed % 3 ===0){
+//      console.log('buzz')
+//   }
+                      
+
+                            // task2
+
+
+// let password=1214
+// let balans=500
+// let righOfEntry=3
+
+
+// let passwordentry = prompt('Zehmet olmasa parolu daxil edin ')
+//     if(password == passwordentry){
+//         let amount = prompt('Cekmek istediyiniz meblegi daxil edin ')
+//         if(amount <= balans){
+//             balans -= amount
+//             alert("Qalan mebleginiz: " + balans)
+            
+
+//         }else{
+//             alert('Kifayet qeder balans yoxdur')
+//         }
+//     }else{
+//         righOfEntry--;
+//         alert('Sehv parol qalan giris haqqiviz ' + righOfEntry)
+//     }
+//     if(righOfEntry == 0){
+//             alert("3 sehv parol yazdiniz hesbiniz bloklandi")
+//             }
+
+                    // Task3
+
+    //          function cem(num1 , num2){
+    //     console.log( num1 + num2)
+    // }
+    // cem(12,13)
+
+
+    // function pow (num1 ,num2){
+    //    return num1**num2
+    // }
+    // console.log(Math.pow(3,4))
+
+                            // task4
+    
+        //   let num1 = +prompt("eded 1")
+        //     let num2 = +prompt("eded 2")
+        //     const pow = (num1 , num2 ) => console.log(num1**num2)
+        //    console.log(pow(num1,num2))
+  
+
+   
+      
+ 
