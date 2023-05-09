2afc384d33bda78de1c49e16543e04f31b33f4f1
Tariyel Sayilov <sayilovtariyel97@gmail.com>
2023-05-09 14:26:36 +0400

    add task

---
+++ app.js
@@ -0,0 +1,129 @@
+               // task1 09.05.2023
+
+// const arr = new Array(5);  // create an array with length 5
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] = Number(prompt(`Enter a number for element ${i+1}:`));  // prompt user for input and convert to number
+// }
+// console.log(arr);   
+
+
+                  // task2
+    // function productOfArray(arr) {
+    // let product = 1;
+    // for (let i = 0; i < arr.length; i++) {
+    //  product *= arr[i];
+    //  }
+    // console.log(product);
+    //  }
+                  
+    //  const numbers = [2, 4, 6, 8];
+    // productOfArray(numbers); 
+
+
+                                // task3
+        
+    //   function multiply() {
+    //    let result = 1;
+    //  for (let i = 0; i < arguments.length; i++) {
+    //       result *= arguments[i];
+    //   }
+    //  return result;
+    //  }
+
+    //  multiply(4,6) ; //24
+    //  multiply(6,5,4);//120
+    //  multiply(8,5,6);//240
+
+
+                                // task4
+    
+    //  function convertElementsToItsLengths(arr) {
+    //  return arr.map(function(str) {
+    //   return str.length;
+    // });
+    //  }
+    //  var arr = ["hello", "world", "how", "are", "you" ,"come"];
+    //  var lengths = convertElementsToItsLengths(arr);
+    //  console.log(lengths); // Output: [5, 5, 3, 3, 3]
+
+                                // task5
+
+//  function findMax(arr) {
+//    return Math.max(...arr);
+// }
+                                  
+// const myArray = [2, 4, 5, 10, 7];
+// const largestElement = findMax(myArray);
+// console.log(largestElement); // Output: 8
+                         
+                            // task6
+
+//    function findLongest(arr) {
+//    let longest = "";
+//    for (let i = 0; i < arr.length; i++) {
+//    if (arr[i].length > longest.length) {
+//    longest = arr[i];
+//    }
+//    }
+//   return longest;
+//   }
+//   const strings = ["apple", "banana", "cherry", "date", "elderberry"];
+//   const longestString = findLongest(strings);
+//   console.log(longestString); // "elderberry"
+
+
+                            // task7
+    //   function cloneArray(arr) {
+    //    return [...arr];
+    //   }
+    //   const originalArray = [1, 2, 3];
+    //   const clonedArray = cloneArray(originalArray);
+    //   console.log(clonedArray); // Output: [1, 2, 3]
+
+                                // task8
+
+    //  function convertMilesToKilometers(milesArray) {
+    //  const conversionFactor = 1.60934; // 1 mile = 1.60934 kilometers
+    //  const kilometersArray = milesArray.map(miles => miles * conversionFactor);
+    //  return kilometersArray;
+    //  }
+    //  const milesArray = [1, 5, 10, 25];
+    //  const kilometersArray = convertMilesToKilometers(milesArray);
+    //  console.log(kilometersArray); // [1.60934, 8.0467, 16.0934, 40.2336]
+     
+                                //   task9
+        
+    //  function letterFromPosition(position) {  
+    //  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");4
+
+    //     const index = position - 1;
+                                          
+    //   return alphabet[index];
+    // }
+    // console.log(letterFromPosition(1));   //Output: "a"
+    // console.log(letterFromPosition(26)); // Output: "z"
+    // console.log(letterFromPosition(20)); // Output: "t"
+                             
+                                // task10
+
+    //    function getEvenNumbers(arr) {
+    //    let evenNumbers = [];
+    //    for (let i = 0; i < arr.length; i++) {
+    //    if (arr[i] % 2 === 0) {
+    //    evenNumbers.push(arr[i]);
+    //    }
+    //      }
+    //     return evenNumbers;
+    //   }
+
+    //   let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    //   let evenNumbers = getEvenNumbers(numbers);
+    //   console.log(evenNumbers); // [2, 4, 6, 8]
+
+                                  
+                                  
+
+                              
+
+                              
+                                  
